34ef34ad50d8a2071c54228ae2ed2bd44a43d989
shakil m akram <shakil@pop-os.localdomain>
2020-10-18 16:55:42 -0400

    changed bigquery schema

--- bigquery.py
+++ bigquery.py
@@ -1,15 +1,17 @@
 from google.cloud import bigquery
+from datetime import datetime
 import os
 
 def Update_deployment(data):
     bq = bigquery.Client()
     table      = os.getenv('bigquery_table')
     dataset    = os.getenv('bigquery_dataset')
+    date       = datetime.today().strftime("%Y-%m-%d")
     update = (
         f"UPDATE {data['project']}.{dataset}.{table} "
         f"SET cpu_type = '{data['machine_type']}',deployment_status = '{data['deployment_status']}'"
         f",job_status = '{data['job_status']}', instance_ip = '{data['instance_ip']}', deployed_on = '{data['deployed_on']}' "
-        f"WHERE instance_name = '{data['instance_name']}' AND zone = '{data['zone']}'"
+        f"WHERE instance_name = '{data['instance_name']}' AND _PARTITIONTIME = '{date}' "
     )
     result =  bq.query(update)
     result.result()
